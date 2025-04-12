b82cc553e2510c846837ea0b225753ea575b816e
linskin <1360293790@qq.com>
2025-04-12 22:54:11 +0800

    初始化

---
+++ src/main/java/com/example/taskmanager/model/Task.java
@@ -0,0 +1,8 @@
+package com.example.taskmanager.model;
+
+/**
+ * 任务实体类
+ */
+public class Task {
+
+}
---
+++ src/main/java/com/example/taskmanager/model/TaskStatus.java
@@ -0,0 +1,8 @@
+package com.example.taskmanager.model;
+
+/**
+ * 任务状态枚举
+ */
+public enum TaskStatus {
+
+}
